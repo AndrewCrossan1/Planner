d196552b6e455f5fc61ebbf0749f4e3b1b7413aa
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-07-17 12:25:44 +0100

    Added Core to INSTALLED_APPS and fixed minor issue with static files

--- Planner/settings.py
+++ Planner/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/',
 ]
 
 MEDIA_URL = 'media/'
