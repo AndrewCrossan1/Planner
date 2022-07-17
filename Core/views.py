c57810d770440ef5bf87e1de51bb925ceac7259c
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-07-17 12:27:22 +0100

    Added view handlers for index, contact, about

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# View creation starts here
+
+
+def index(request):
+    page_name = 'home'  # Used for setting active link in navigation
+    return render(request, 'Core/home/index.html', context={'page_name': page_name})
+
+
+def contact(request):
+    page_name = 'contact'  # Used for setting active link in navigation
+    # TODO: Implement contact form
+    return render(request, 'Core/contact/contact.html', context={'page_name': page_name})
+
+
+def about(request):
+    page_name = 'about'  # Used for setting active link in navigation
+    return render(request, 'Core/about/about.html', context={'page_name': page_name})
